9490afb805f1cf6645afc903d2ce74af033b3dc5
Jaewoo Kim <wodndhksk@gmail.com>
2024-10-27 21:17:03 +0900

    first commit

---
+++ src/test/java/io/securiy/springsecuritymaster/SpringsecuritymasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.securiy.springsecuritymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritymasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
